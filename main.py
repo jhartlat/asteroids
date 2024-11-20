ff633e09650cd5595e096988b6d0939fd5c19e7c
jhartlat <jhart.lat@gmail.com>
2024-11-19 23:08:12 -0600

    create the game loop to draw the game onto the screen

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
-import constants
+from constants import *
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
