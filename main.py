811e718101f5c913d1bdc2707d36f2a9409d18b8
jhartlat <jhart.lat@gmail.com>
2024-12-08 02:54:01 -0600

    refactor main to draw triangle on display

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+
+    # Initialize pygame
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Set up the game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    # Set up game clock
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Set up the game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
+        # Clear the screen
+        pygame.Surface.fill(screen, 'black')
+
+        # Draw the triangle
+        x_pos = SCREEN_WIDTH / 2
+        y_pos = SCREEN_HEIGHT / 2
+        player1 = Player(x=x_pos, y=y_pos)
+        player1.draw(screen)
 
         # Convert delta_time from miliseconds to seconds
         dt = clock.tick(60) / 1000
 
-
-
+        # Update the display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
