03a7e4cdbae3d15e2bed1358758a50176d10cba2
jhartlat <jhart.lat@gmail.com>
2024-12-09 16:26:46 -0600

    override draw and update methods

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,14 @@
 from circleshape import *
 
 class Asteroid(CircleShape):
-    pass
+
+    containers = None
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', (self.x, self.y), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
